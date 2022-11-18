ba5b0373ebde7bc2dddb1c58d8d28e294c2fb5de
Gregor Rammal <gregor.rammal@gmail.com>
2022-11-18 18:47:30 +0200

    Taskide tabel

--- script_oliver.js
+++ script_oliver.js
@@ -1,39 +1,3 @@
-
-const task_form = document.getElementById('task_form');
-const del = document.getElementById('del');
-
-
-task_form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	let name = task_form.elements["task_name"].value;
-	let date = task_form.elements["task_date"].value;
-	let cat = task_form.elements["task_cat"].value;
-	let desc = task_form.elements["task_desc"].value;
-
-	let array = [name, date, cat, desc];
-
-	localStorage.setItem(name, array);
-	printTasks()
-});
-
-
-function printTasks(){
-	let str = "";
-	for (i = 0; i < localStorage.length; i++)   {
-		let name = localStorage.key(i);
-		array = localStorage.getItem(name);
-		str += array + "<br>";
-	};
-	paragraph = document.getElementById("loc");
-	paragraph.innerHTML = str;
-};
-
-del.addEventListener('click', (event) => {
-	localStorage.clear();
-	paragraph = document.getElementById("loc");
-	paragraph.innerHTML = "";
-});
-
 const date = new Date();
 
 const renderCalendar = () => {
@@ -116,6 +80,3 @@ document.querySelector(".next").addEventListener("click", () => {
 });
 
 renderCalendar();
-printTasks()
-
-   
